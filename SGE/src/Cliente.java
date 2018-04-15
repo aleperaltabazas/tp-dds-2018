d8d856f177a75d602f988f6abfce3ea958d06295
gonzovaq <gonzovaq@gmail.com>
2018-04-15 01:03:56 -0300

    Se inicia el proyecto con maven

---
+++ SGE/src/Cliente.java
@@ -0,0 +1,15 @@
+//	import org.json.*;
+
+public class Cliente {
+	JSONObject obj = new JSONObject("{\r\n" + 
+			"	\"nombre\": \"gonza\",\r\n" + 
+			"	\"apellido\": \"vaquero\",\r\n" + 
+			"	\"tipoDni\": \"DNI\",\r\n" + 
+			"	\"numeroDocumento\"; \"666\",\r\n" + 
+			"	\"telefono\": \"12345678\",\r\n" + 
+			"	\"domicilio\": \"calle false 123\",\r\n" + 
+			"	\"fecha\": \"14/06/2300\",\r\n" + 
+			"	\"categoria\": \"R1\"\r\n" + 
+			"}");
+	String nombre = obj.getJSONObject("nombre").getString("nombre");	
+}
