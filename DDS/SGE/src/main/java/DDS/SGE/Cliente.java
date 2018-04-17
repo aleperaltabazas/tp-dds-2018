1f2f3b8110745c0617b92b070b22f4a5be4dd460
lusalmeron <lucila@salmeron.com.ar>
2018-04-17 16:09:45 -0300

    Agrego categorias como ENUMs. Falta adaptar Cliente y resolver tipoDeCargo Fijo o Variable

--- DDS/SGE/src/main/java/DDS/SGE/Cliente.java
+++ DDS/SGE/src/main/java/DDS/SGE/Cliente.java
@@ -11,7 +11,17 @@ public class Cliente {
 		"	\"telefono\": \"12345678\",\r\n" + 
 		"	\"domicilio\": \"calle false 123\",\r\n" + 
 		"	\"fecha\": \"14/06/2300\",\r\n" + 
-		"	\"categoria\": \"R1\"\r\n" + 
+		"	\"categoria\": \"R1\"\r\n" + /// La variable debería ser del tipo category categoria (ENUM)
 		"}");
 	String nombre = obj.getJSONObject("nombre").getString("nombre");	
+	
+	public void setCategoria(category nuevaCategoria) {
+		categoria = nuevaCategoria; /// Agregar variable
+	}
+	
+	/// PARA QUE DEVUELVA LA SUMA DE LOS CONSUMOS DE TODOS SUS DISPOSITIVOS
+	public double consumoTotalPorHora() {
+		///...
+		return 1; /// Desarrollar
+	};
 }
