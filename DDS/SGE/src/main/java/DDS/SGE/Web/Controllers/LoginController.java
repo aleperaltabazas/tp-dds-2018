4835cd7197adfe3446268c7c038c48af72020333
Alejandro <aleperaltabazas@hotmail.com>
2018-10-28 14:53:00 -0300

    correcciones durante entrega

--- DDS/SGE/src/main/java/DDS/SGE/Web/Controllers/LoginController.java
+++ DDS/SGE/src/main/java/DDS/SGE/Web/Controllers/LoginController.java
@@ -1,5 +1,8 @@
 package DDS.SGE.Web.Controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import DDS.SGE.RepositorioUsuarios;
 import DDS.SGE.Usuario;
 import spark.ModelAndView;
@@ -18,24 +21,27 @@ public class LoginController {
 	
 	public static ModelAndView login(Request req, Response res) {
 		String username = req.queryParams("username");
+		String password = req.queryParams("password");
 		
 		Usuario usuario = RepositorioUsuarios.instancia.buscarSegunNombre(username);
-		
+
 		if(username.equals("admin")) {
 			res.redirect("/administrador");
 		}
 		
 		req.session().attribute(SESSION_NAME);
 		
-		if(usuario.getPassword().equals(req.queryParams("password"))) {
+		if(usuario.getPassword().equals(password)) {
 			//Se deberia hacer con el Id
 			res.redirect("/user/" + usuario.getUsername());
 			req.session().attribute(SESSION_NAME, usuario.getUsername());
 			
-		}
-		else {
+		} else {
 			//Se podria mostrar un "error en el login"
-			return new ModelAndView(null, "loginError.hbs");
+			Map<String, Object> viewmodel = new HashMap();
+			viewmodel.put("username", username);
+			viewmodel.put("password", password);
+			return new ModelAndView(viewmodel, "loginError.hbs");
 		}
 	
 		return new ModelAndView(null, "login.hbs");
